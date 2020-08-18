e9dfe9026d5588cbd175d6df020525236e67cec1
Eugene Kim <eugene@MacBook-Pro.local>
2020-08-17 19:34:33 -0700

    Edited background color to indigo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class Loading extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: "Loading",
+    };
+  }
+  componentDidMount() {
+    const stopper = this.state.text + "...";
+
+    this.interval = window.setInterval(() => {
+      this.state.text === stopper
+        ? this.setState(() => ({ text: "Loading" }))
+        : this.setState((prevState) => ({ text: prevState.text + "." }));
+    }, 300);
+  }
+  componentWillUnmount() {
+    window.clearInterval(this.interval);
+  }
+  render() {
+    return <p>{this.state.text}</p>;
+  }
+}
 
 function App() {
   return (
@@ -8,15 +33,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Loading />
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/channel/UChV9LIbhVgQoROuFZK_wmPQ"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          sayvibes
         </a>
       </header>
     </div>
